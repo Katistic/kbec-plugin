90da4032acfb4b20d47ae2a8fbeeaa67b3ffcfd0
Kat <kat@vocreatures.com>
2025-03-09 18:53:19 +1100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="kbec_plugin",
     packages=["kbec_plugin"],
-    version="0.3",
+    version="0.4",
     license="GPL-3.0",
     description="Plugin framework for interacting with Battleye RCON",
     author="Katsi",
