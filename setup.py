a862f671a8c6986bd2c2035e129bfbd4cb2f40ed
Kat <kat@vocreatures.com>
2025-03-09 19:47:51 +1100

    Typo (again)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="kbec_plugin",
     packages=["kbec_plugin"],
-    version="0.4.1",
+    version="0.4.2",
     license="GPL-3.0",
     description="Plugin framework for interacting with Battleye RCON",
     author="Katsi",
